eccadb2e4bf4f6c0453dde0dad87581f7005cbc4
DikaAyuWijayanti <dikaayuwijayanti1234@gmail.com>
2021-12-11 03:57:16 +0700

    edit file docker

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
 $namaHost = "localhost";
 $username = "root";
-$password = "";
+$password = "admin";
 $database = "website_dika";
 
 $connect = mysqli_connect($namaHost, $username, $password, $database);
